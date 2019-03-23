422ba0f13494836a841afcd908981de0a9e1b46c
Sergey Lobin <sergey.lobin@gmail.com>
2019-03-23 12:32:24 +0300

    Steps 19

--- make-app.mk
+++ make-app.mk
@@ -9,6 +9,9 @@ app-build:
 app-bash:
 	docker-compose run -p 3000:8080 --user=$(USER) app bash
 
+app-setup: development-setup-env app-build
+	docker-compose run --user=$(USER) app
+
 development-setup-env:
 	ansible-playbook ansible/development.yml -i ansible/development -vv
 			
