80d45444a4be8968833d0e2905d9ce28de950a4a
Sergey Lobin <sergey.lobin@gmail.com>
2019-03-24 19:35:58 +0300

    Added ping & cleanup

--- make-prod.mk
+++ make-prod.mk
@@ -1,8 +1,11 @@
+production-ping:
+	ansible-playbook ansible/deploy.yml -i ansible/production -t ping -vv --vault-password-file ../../vault.txt	
+
 production-setup:
-	ansible-playbook ansible/setup-docker.yml -i ansible/production -vv --vault-password-file ../../vault.txt
+	ansible-playbook ansible/setup-docker.yml -i ansible/production --vault-password-file ../../vault.txt
 
 production-deploy:
-	ansible-playbook ansible/deploy.yml -i ansible/production -vv --vault-password-file ../../vault.txt
+	ansible-playbook ansible/deploy.yml -i ansible/production --vault-password-file ../../vault.txt
 
 production-deploy-app:
-	ansible-playbook ansible/deploy.yml -i ansible/production -t app -vv --vault-password-file ../../vault.txt	
+	ansible-playbook ansible/deploy.yml -i ansible/production -t app --vault-password-file ../../vault.txt	
