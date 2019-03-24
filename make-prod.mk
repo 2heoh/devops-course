9a133f3dd312f1f7b5ab8a769eade9f3eee8dd99
Sergey Lobin <sergey.lobin@gmail.com>
2019-03-24 19:07:54 +0300

    cleanup

--- make-prod.mk
+++ make-prod.mk
@@ -1,5 +1,5 @@
 production-setup:
-	ansible-playbook ansible/setup.yml -i ansible/production -vv --vault-password-file ../../vault.txt
+	ansible-playbook ansible/setup-docker.yml -i ansible/production -vv --vault-password-file ../../vault.txt
 
 production-deploy:
 	ansible-playbook ansible/deploy.yml -i ansible/production -vv --vault-password-file ../../vault.txt
