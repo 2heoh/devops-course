7fae6162459d3249e20e83a3a76f16aa2ee55cb1
Sergey Lobin <sergey.lobin@gmail.com>
2019-03-24 11:41:42 +0300

    Step 33-35

---
+++ make-prod.mk
@@ -0,0 +1,8 @@
+production-setup:
+	ansible-playbook ansible/setup.yml -i ansible/production -vv --vault-password-file ../../vault.txt
+
+production-deploy:
+	ansible-playbook ansible/deploy.yml -i ansible/production -vv --vault-password-file ../../vault.txt
+
+production-deploy-app:
+	ansible-playbook ansible/deploy.yml -i ansible/production -t app -vv --vault-password-file ../../vault.txt	
